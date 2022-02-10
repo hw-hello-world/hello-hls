5105b688cd2d6b119b85a112530b726d535ee942
Haisheng Wu <haisheng.wu@okta.com>
2022-02-09 17:33:49 -0800

    add comment for how to import unknown function

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let a = foo
+  putStrLn "Hello, Haskell!"
+
+foo :: Integer
+foo = fromMaybe 0 Nothing -- to import fromMaybe, try space c l -> a a, which shows list of possible imports
